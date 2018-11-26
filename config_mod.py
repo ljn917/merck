008ede87a837fbcb938db106e0d58e02f5ac5207
Jiannan Liu <ljn917@gmail.com>
2018-11-26 17:04:22 -0500

    Add path in config_mod

---
+++ config_mod.py
@@ -0,0 +1,3 @@
+data_root = '/mnt/DATA/keras/merck_dataset/'
+save_root = '/mnt/DATA/keras/merck_dataset/preprocessed/'
+dataset_names = ['3A4', 'CB1', 'DPP4', 'HIVINT', 'HIVPROT', 'LOGD', 'METAB', 'NK1', 'OX1', 'OX2', 'PGP', 'PPB', 'RAT_F', 'TDI', 'THROMBIN']
